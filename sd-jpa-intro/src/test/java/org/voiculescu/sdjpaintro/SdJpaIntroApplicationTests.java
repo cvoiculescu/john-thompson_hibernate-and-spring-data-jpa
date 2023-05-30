8816b1ec9483ba37f21fa07d5c15170e21beba67
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-30 10:46:59 +0300

    Section4: Introduction to Testing with SpringBoot

--- sd-jpa-intro/src/test/java/org/voiculescu/sdjpaintro/SdJpaIntroApplicationTests.java
+++ sd-jpa-intro/src/test/java/org/voiculescu/sdjpaintro/SdJpaIntroApplicationTests.java
@@ -1,10 +1,23 @@
 package org.voiculescu.sdjpaintro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.voiculescu.sdjpaintro.repository.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdJpaIntroApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void testBookRepository(){
+        long count = bookRepository.count();
+        assertThat(count).isGreaterThan(0);
+    }
 
     @Test
     void contextLoads() {
---
+++ sd-jpa-intro/src/test/java/org/voiculescu/sdjpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,46 @@
+package org.voiculescu.sdjpaintro;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.voiculescu.sdjpaintro.domain.Book;
+import org.voiculescu.sdjpaintro.repository.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+@ComponentScan(basePackages = {"org.voiculescu.sdjpaintro.bootstrap"})
+class SpringBootJpaTestSlice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Commit
+    @Order(1)
+    @Test
+    public void testJpaTestSlice(){
+        long countBefore = bookRepository.count();
+        bookRepository.save(new Book("MyBook","isbn","MyPublisher"));
+        long countAfter = bookRepository.count();
+        assertThat(countAfter).isGreaterThan(countBefore);
+    }
+
+    @Order(2)
+    @Test
+    public void testJpaTestSliceTransactional(){
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+        bookRepository.save(new Book("MyBook1","isbn1","MyPublisher1"));
+        long countAfter = bookRepository.count();
+        assertThat(countAfter).isGreaterThan(countBefore);
+    }
+
+}

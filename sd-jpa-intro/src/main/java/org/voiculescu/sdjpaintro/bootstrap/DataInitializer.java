8f7e2ca521b7e17e93eaf07c1c6106916e315be8
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-30 11:13:37 +0300

    Section5: Hibernate with MySQL -> Integration with MySQL

--- sd-jpa-intro/src/main/java/org/voiculescu/sdjpaintro/bootstrap/DataInitializer.java
+++ sd-jpa-intro/src/main/java/org/voiculescu/sdjpaintro/bootstrap/DataInitializer.java
@@ -18,12 +18,14 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if (bookRepository.count() > 0) {
+            return;
+        }
         Book bookDDD = new Book().setTitle("Domain Driven Design").setPublisher("Random House");
-        log.info("{} -> {}",bookDDD.getId(),bookDDD.getTitle());
-        Book savedDDD = bookRepository.save(bookDDD);
+
+        bookRepository.save(bookDDD);
         Book bookSIA = new Book().setTitle("Spring in Action").setPublisher("O'Reilly");
-        log.info("{} -> {}",bookSIA.getId(),bookSIA.getTitle());
-        Book savedSIA = bookRepository.save(bookSIA);
-        bookRepository.findAll().forEach(book->log.info("{} -> {}",book.getId(),book.getTitle()));
+
+        bookRepository.save(bookSIA);
     }
 }

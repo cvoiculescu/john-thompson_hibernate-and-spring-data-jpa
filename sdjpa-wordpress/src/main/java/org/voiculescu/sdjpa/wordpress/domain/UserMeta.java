216694e2f7eb5869eaccd66b669715556e5fbee9
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-06-08 14:39:26 +0300

    Section21: Legacy Data Mapping - UserMeta - LOB

---
+++ sdjpa-wordpress/src/main/java/org/voiculescu/sdjpa/wordpress/domain/UserMeta.java
@@ -0,0 +1,35 @@
+package org.voiculescu.sdjpa.wordpress.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Size(max = 255)
+    @Column(name = "meta_key")
+    private String metaKey;
+
+    @Lob
+    @Column(name = "meta_value")
+    @JdbcTypeCode(SqlTypes.LONGVARCHAR)
+    private String metaValue;
+
+}

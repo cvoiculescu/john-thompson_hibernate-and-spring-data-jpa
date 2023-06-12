b1f8b5d826fbcc6e384120bb78b5b6ffa30f0048
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-06-12 13:12:13 +0300

    Section26: Spring DATA Rest - Customize URL path

--- sdjpa-spring-data-rest/src/main/java/org/voiculescu/sdjpa/springdatarest/repositories/BeerRepository.java
+++ sdjpa-spring-data-rest/src/main/java/org/voiculescu/sdjpa/springdatarest/repositories/BeerRepository.java
@@ -3,11 +3,14 @@ package org.voiculescu.sdjpa.springdatarest.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.voiculescu.sdjpa.springdatarest.domain.Beer;
 import org.voiculescu.sdjpa.springdatarest.domain.BeerStyleEnum;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
